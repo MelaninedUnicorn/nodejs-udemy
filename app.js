df7e58ffb7b63c403f5da0b9e2d5c8a2740cfe3d
Mugisha J Kakou <mugisha@joycava.com>
2020-11-23 11:56:05 +0100

    handling routes -section 5 node class

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require('http');
-
-const routes = require('./routes');
 
 
-const server = http.createServer(routes);
+ const express = require('express');
+
+//  this is a valid request handler
+ const app = express()
+
+ app.use('/product',(req,res,next) => {
+    console.log("In another middleware");
+    res.send('<h1>Hello from "add product" Mugisha!</h1>')
+});
+
+ app.use('/',(req,res,next) => {
+    console.log("In another middleware");
+    res.send('<h1>Hello from express Mugisha!</h1>')
+});
 
 
-server.listen(3000);
+
+app.listen(3000);

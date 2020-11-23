6b86c9a48b402a6c94cb7ff27ddcbbbce76b9773
Mugisha J Kakou <mugisha@joycava.com>
2020-11-23 13:38:54 +0100

    72. using a helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
 
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
    
 });
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,13 @@ const e = require('express');
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
 module.exports = router;

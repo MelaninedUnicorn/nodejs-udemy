29a0a073cace059f16eff077cab4aac9cfd6a364
Mugisha J Kakou <mugisha@joycava.com>
2020-11-30 18:16:26 +0100

    84. Adding a Layout

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
 
     // res.sendFile(path.join(rootDir,'views','add-product.html'))
-    res.render('add-product',{docTitle: 'Add Product'});
+    res.render('add-product',{pageTitle: 'Add Product', path:"/admin/add-product"});
 });
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {

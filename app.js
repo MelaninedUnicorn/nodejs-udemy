007aa643aa269d84a20b94aba06216e4aca87818
Mugisha J Kakou <mugisha@joycava.com>
2020-12-03 18:56:11 +0100

    91. Working on the Layout with Partials

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 
 app.use('/',(req,res,next) => {
     
-res.status(404).render('404',{pageTitle:"Page Not Found"});
+res.status(404).render('404',{pageTitle:"Page Not Found", path: false});
 });
 
 

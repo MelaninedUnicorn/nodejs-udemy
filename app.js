d66660559b6f6fe7856296166e01f9c5d3269ba7
Mugisha J Kakou <mugisha@joycava.com>
2020-12-03 18:25:26 +0100

    90. Working with EJS

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const expressHbs = require('express-handlebars');
+ 
  
 const app = express();
-
-app.engine('hbs',expressHbs({layoutsDir: 'views/layouts/',defaultLayout: 'main-layout', extname:'hbs'}));
-app.set('view engine','hbs');
+ 
+app.set('view engine','ejs');
 // if your views were stored else where you would put 
 app.set('views','views');
 

14fab9ec9a12c539adf2db00dd822a0936bde6f3
Mugisha J Kakou <mugisha@joycava.com>
2020-12-04 14:52:23 +0100

    106. Creating the Shop Structure

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir,'views','add-product.html'))
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCss: true,
@@ -15,13 +15,13 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
     const product = new Product(req.body.title);
     product.save();
-  res.redirect("/");
+  res.redirect('/');
 };
 
 exports.getProduct = (req, res, next) => {
 
     Product.fetchAll((products)=>{ 
-        res.render("shop", {
+        res.render("shop/product-list", {
             prods: products,
             pageTitle: "Shop",
             path: "/",
